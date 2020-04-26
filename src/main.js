ff794efd3b9557c9822731744a9c562e72edc7a4
Kristoffer Pudas <kristofferpudas@gmail.com>
2020-04-26 17:10:32 +0200

    Post entries client side

--- src/main.js
+++ src/main.js
@@ -1,3 +1,7 @@
+const tagsElement = document.getElementById('tags')
+const contentElement = document.getElementById('content')
+const entriesElement = document.getElementById('entries')
+
 async function fetchJSON(url) {
   return fetch(url)
     .then(response => response.json())
@@ -15,18 +19,53 @@ function Entry(tags, content) {
 function EntryView(entry) {
   return `
     <div class="entry">
-      <div class="date">${(new Date(entry.dateEdited)).toDateString()}</div>
+      <div class="date">${(new Date(entry.dateEdited || entry.dateCreated)).toDateString()}</div>
       <div class="tags" contenteditable>${entry.tags.join(', ')}</div>
       <div class="content" contenteditable>${entry.content}</div>
     </div>
   `
 }
 
+function addEntry(element, entry) {
+  element.innerHTML += EntryView(entry)
+}
+
 function listEntries(element, entries) {
   for (const entry of entries) {
-    element.innerHTML += EntryView(entry)
+    addEntry(element, entry)
   }
 }
 
+function addContentElementListeners() {
+  let controlIsDown = false
+
+  contentElement.addEventListener('keydown', e => {
+    if (e.key === 'Control') {
+      controlIsDown = true
+    } else if (e.key === 'Enter' && controlIsDown) {
+      const tags = tagsElement.value
+        .split(',')
+        .map(tag => tag.trim())
+        .filter(tag => tag.length > 0)
+
+      const content = contentElement.value
+
+      if (content.length > 0) {
+        const entry = Entry(tags, content)
+        addEntry(entriesElement, entry)
+        contentElement.value = ''
+      }
+    }
+  })
+
+  contentElement.addEventListener('keyup', e => {
+    if (e.key === 'Control') {
+      controlIsDown = false
+    }
+  })
+}
+
+addContentElementListeners()
+
 fetchJSON('/entries').then(data =>
-  listEntries(document.getElementById('entries'), data.entries))
+  listEntries(entriesElement, data.entries))
