680b5e6e181abb128c3125b696836f7bdd29d1cb
Kristoffer Pudas <kristofferpudas@gmail.com>
2020-04-26 15:40:13 +0200

    List entries

--- src/main.js
+++ src/main.js
@@ -1,12 +1,32 @@
-let state = null
-
 async function fetchState() {
   return fetch('/state')
     .then(response => response.json())
 }
 
-function updateState() {
-  fetchState().then(s => state = s)
+function Entry(tags, content) {
+  return {
+    tags,
+    content,
+    dateCreated: Date.now(),
+    dateEdited: null
+  }
 }
 
-updateState()
+function EntryView(entry) {
+  return `
+    <div class="entry">
+      <div class="date">${(new Date(entry.dateEdited)).toDateString()}</div>
+      <div class="tags" contenteditable>${entry.tags.join(', ')}</div>
+      <div class="content" contenteditable>${entry.content}</div>
+    </div>
+  `
+}
+
+function list(element, entries) {
+  for (const entry of entries) {
+    element.innerHTML += EntryView(entry)
+  }
+}
+
+fetchState().then(state =>
+  list(document.getElementById('entries'), state.entries))
