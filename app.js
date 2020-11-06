aba3d7de981de2a7534c466827c1ae4da95d04d6
Kristoffer Pudas <kristofferpudas@gmail.com>
2020-11-06 11:28:18 +0100

    Rename public directory

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.use(express.static('src'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, 'src', 'index.html')))
+  res.sendFile(path.join(__dirname, 'public', 'index.html')))
 
 app.use(express.json())
 
